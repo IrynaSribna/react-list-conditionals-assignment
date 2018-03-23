85e6cef908f51601fdb961a0dec9111249df75f5
iryna.sribna <sribna@gmail.com>
2018-03-23 11:26:20 +0100

    Remove unused inputLength variable

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Char from './Char/Char'
 import Validation from './Validation/Validation'
 
 class App extends Component {
   state = {
-    len: 0,
-    text: '',
-    show: true
+    text: ''
   }
   
   inputHandler = (inputText) => {
-    const inputLength = inputText.length
     this.setState(
       {
-        len: inputLength, 
         text: inputText
       })
   }
 
-  deleteCharHandler = (char, charIndex) => {
+  deleteCharHandler = (charIndex) => {
     const currentText = this.state.text
     const updatedText = currentText.slice(0, charIndex) 
                         + currentText.slice(charIndex+1)
-    this.setState({text: updatedText, show: false})
+    this.setState({text: updatedText})
   }
 
   render() {
@@ -35,21 +30,22 @@ class App extends Component {
       padding: '8px',
       margin: '20px'
     }
-    let seqT = this.state.text.split('');
+    
+    let charList = this.state.text.split('')
+        .map((ch, charIndex) => 
+          <Char char={ch} 
+                clicked={() => this.deleteCharHandler(charIndex)}
+                key={charIndex}/>);
+    
     return (
-      
       <div className="App">
         <input style={style} onChange={(event) => 
           this.inputHandler(event.target.value)} 
           type="text"
           value={this.state.text}/>
         
-        <p>Lenght: {this.state.len}</p>
-        <Validation textLength={this.state.len}/>
-        {seqT.map((ch, charIndex) => 
-          <Char char={ch} 
-            clicked={() => this.deleteCharHandler(ch, charIndex)}
-            key={charIndex}/>)}
+        <Validation textLength={this.state.text.length}/>
+        {charList}
       </div>  
     );
   }
